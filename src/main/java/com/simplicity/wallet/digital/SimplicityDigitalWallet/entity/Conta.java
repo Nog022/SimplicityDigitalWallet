81ac20d1c1e0ce848498082915f95d54e893e156
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-05 17:34:37 -0300

    Criando classe Conta

--- src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/entity/Conta.java
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/entity/Conta.java
@@ -2,7 +2,12 @@ package com.simplicity.wallet.digital.SimplicityDigitalWallet.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -10,6 +15,13 @@ public class Conta {
 
     @Id
     private Long id;
+    private Long numeroConta;
+    private List<Transacao> transacoes;
+    private LocalDateTime dataCriacao;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
 
 }
