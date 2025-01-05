7c80bee771de076256e953077b78f05b6aa4f8cc
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2025-01-05 16:29:33 -0300

    apagando classes

---
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+package com.simplicity.wallet.digital.SimplicityDigitalWallet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+
+
+}
