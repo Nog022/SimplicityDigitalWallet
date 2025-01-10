e17c975f635500721c6820baea72a74429f56b6c
Breno Braido <81990426+BrenoSB98@users.noreply.github.com>
2025-01-10 14:54:31 -0300

    Implementa as Interfaces repository e service para Saque

---
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/repository/SaqueRepository.java
@@ -0,0 +1,7 @@
+package com.simplicity.wallet.digital.SimplicityDigitalWallet.repository;
+
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Saque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaqueRepository extends JpaRepository<Saque, Long> {
+}
---
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/service/SaqueService.java
@@ -0,0 +1,7 @@
+package com.simplicity.wallet.digital.SimplicityDigitalWallet.service;
+
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.dto.SaqueDTO;
+
+public interface SaqueService {
+    void realizarSaque(SaqueDTO saqueDTO);
+}
