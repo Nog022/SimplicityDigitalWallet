c45eef7b0717fbde951654027a711ee96888458c
Breno Braido <81990426+BrenoSB98@users.noreply.github.com>
2025-01-10 15:28:38 -0300

    Implementação de SaqueServiceImpl

--- src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/repository/SaqueRepository.java
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/repository/SaqueRepository.java
@@ -2,6 +2,8 @@ package com.simplicity.wallet.digital.SimplicityDigitalWallet.repository;
 
 import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Saque;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaqueRepository extends JpaRepository<Saque, Long> {
 }
---
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/service/impl/SaqueServiceImpl.java
@@ -0,0 +1,43 @@
+package com.simplicity.wallet.digital.SimplicityDigitalWallet.service.impl;
+
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.dto.SaqueDTO;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Conta;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Saque;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.exceptions.ContaNaoEncontradaException;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.repository.ContaRepository;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.repository.SaqueRepository;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.service.SaqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class SaqueServiceImpl implements SaqueService {
+
+    @Autowired
+    private SaqueRepository saqueRepository;
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+    @Override
+    public void realizarSaque(SaqueDTO saqueDTO) {
+        Conta conta = contaRepository.findByNumeroConta(saqueDTO.idConta())
+                .orElseThrow(() -> new ContaNaoEncontradaException("Conta não encontrada para o ID: " + saqueDTO.idConta()));
+
+        BigDecimal saldoAtual = conta.getSaldo();
+        if (saldoAtual.compareTo(saqueDTO.valor()) < 0) {
+            throw new IllegalArgumentException("Saldo insuficiente para realizar o saque.");
+        }
+
+        conta.setSaldo(saldoAtual.subtract(saqueDTO.valor()));
+        contaRepository.save(conta);
+
+        Saque saque = new Saque();
+        saque.setValor(saqueDTO.valor());
+        saque.setDataSaque(saqueDTO.dataSaque());
+        saque.setIdConta(conta);
+        saqueRepository.save(saque);
+    }
+}
