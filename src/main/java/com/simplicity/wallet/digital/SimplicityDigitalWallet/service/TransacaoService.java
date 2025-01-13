a26d9c5e275312ccd629385721b78777395084e6
Breno Braido <81990426+BrenoSB98@users.noreply.github.com>
2025-01-13 17:26:45 -0300

    Atualiza TransacaoService, criando metodo transacao(Saque saque)

--- src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/service/TransacaoService.java
+++ src/main/java/com/simplicity/wallet/digital/SimplicityDigitalWallet/service/TransacaoService.java
@@ -2,6 +2,7 @@ package com.simplicity.wallet.digital.SimplicityDigitalWallet.service;
 
 import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Conta;
 import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Deposito;
+import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Saque;
 import com.simplicity.wallet.digital.SimplicityDigitalWallet.entity.Transacao;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 @Service
 public interface TransacaoService {
 
+    void transacao(Saque saque);
     void transacao(Deposito deposito);
     void transacao(Conta contaOrigem, Conta contaDestino, BigDecimal valor);
 }
